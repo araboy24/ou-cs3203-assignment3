97f9f1afdbb0e09f9295ad24780a5d267c0cbb24
eidza <eidzaben24@gmail.com>
2023-02-10 22:32:19 -0600

    main function added

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ def product_list(lst):
     for i in lst:
         product *= i
     return product
+
+def main():
+    response = ""
+    numbers = []
+    while response.lower() != 'x':
+        response = input("Enter a number or x to stop\n")
+        if response.lower() == 'x':
+            break
+        numbers.append(int(response))
+    print(f'Sum of Numbers: {sum_list(numbers)}')
+    print(f'Product of Numbers: {product_list(numbers)}')
+
+if __name__ == "__main__":
+    main()
